79e30319b1f12c3a0ddbaa8f3445a3546ac16c67
Sean McLeaish <smcleaish@gmail.com>
2024-10-30 20:03:22 -0400

    Encapsulation and ownership

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,21 +11,7 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        Self::string_check(&title, &description, &status);
 
         Ticket {
             title,
@@ -44,6 +30,36 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+    pub fn set_title(&mut self, new_title: String) {
+        Self::string_check(&new_title, &self.description, &self.status);
+        self.title = new_title
+    }
+    pub fn set_description(&mut self, new_description: String) {
+        Self::string_check(&self.title, &new_description, &self.status);
+        self.description = new_description
+    }
+    pub fn set_status(&mut self, new_status: String) {
+        Self::string_check(&self.title, &self.description, &new_status);
+        self.status = new_status
+    }
+
+    fn string_check(title: &String, description: &String, status: &String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
     }
 }
 
